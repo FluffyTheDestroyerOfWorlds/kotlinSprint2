95eb26f461d657676080ae2a2f8102f2b351c50c
Dave Boesen <theboesens@comcast.net>
2018-11-13 07:57:47 -0600

    initial commit
    This is the initial commit of the Sprint 2 Kotlin Code and Design.

---
+++ app/src/main/java/com/bigskysolutionsinc/boesen/kotlin_sprint2/MainClassList.kt
@@ -0,0 +1,12 @@
+package com.bigskysolutionsinc.boesen.kotlin_sprint2
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainClassList : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main_class_list)
+    }
+}
